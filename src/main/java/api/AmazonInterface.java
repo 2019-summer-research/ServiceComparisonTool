c34706b7e960c4588763ef1341ffb04b319e31d4
Conrad Weiser <vibrantclouds@protonmail.com>
2019-07-01 12:19:59 -0400

    Template init

--- src/main/java/api/AmazonInterface.java
+++ src/main/java/api/AmazonInterface.java
@@ -1,4 +1,24 @@
 package api;
 
-public class AmazonInterface {
+import api.element.CompareFaceElement;
+import api.element.PersongroupInformationElement;
+
+import java.io.File;
+
+public class AmazonInterface implements ViewportInterface{
+
+	@Override
+	public void trainNetwork(String groupId) {
+
+	}
+
+	@Override
+	public CompareFaceElement compareFace(File localFaceToCompare, String personGroupId) {
+		return null;
+	}
+
+	@Override
+	public PersongroupInformationElement createPersongroup(String persongroupName) {
+		return null;
+	}
 }
